0ab2b4fb9145d731efd5ed5a3df5fe441b540d56
Amartya Aishwarya <amartya970914@gmail.com>
2020-09-08 16:05:58 +0530

    layout modified a bit

--- common.js
+++ common.js
@@ -1,2 +1,7 @@
 const API_KEY = 'b9f983dc';
-const url = `http://www.omdbapi.com/?apikey=${API_KEY}`;   //&s=sholay
+const url = `http://www.omdbapi.com/?apikey=${API_KEY}`;  
+
+var imdbIDs = [];
+
+
+
