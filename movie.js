22053848c5713db881d5e8bf4b9eb570a4c7794d
Amartya Aishwarya <amartya970914@gmail.com>
2020-09-08 15:02:57 +0530

    handled indiviudal movie loading

--- movie.js
+++ movie.js
@@ -1,1 +1,82 @@
-function  
+
+const movieContainer = document.querySelector('#movie_container');
+
+//fetching individual movie through id
+function getQueryParameter(param){
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+async function fetchMovie(id){
+  // console.log("Amar");
+  let searchQuery = url + '&i=' + id;
+  const response = await fetch(searchQuery).catch(error => console.log(error) );
+  let data = await response.json();
+  console.log(data);
+  return data;
+}
+
+
+
+function makeMovieCard(movie){
+
+    let image = movie.Poster;
+    if (image === 'N/A'){
+      image = "https://sainfoinc.com/wp-content/uploads/2018/02/image-not-available.jpg"
+    }
+    movieContainer.innerHTML = `
+    <div id="movie_disp">
+      <div id="movie_poster">
+        <img src=${image} alt=${movie.Title}>
+      </div>
+      <div id="about_movie">
+        <div id="details">
+
+          <div class="sub_sec">
+            <span class="field">TITLE </span>
+            <span class="value">${movie.Title}</span>
+            <i class="far fa-heart" data-id = ""></i>
+          </div>
+
+          <div class="sub_sec">
+            <span class="field">Year </span>
+            <span class="value">${movie.Year}</span>
+          </div>
+
+          <div class="sub_sec">
+            <span class="field">Genre </span>
+            <span class="value">${movie.Genre}</span>
+          </div>
+
+          <div class="sub_sec">
+            <span class="field">Director </span>
+            <span class="value">${movie.Director}</span>
+          </div>
+
+          <div class="sub_sec">
+            <span class="field">Actors </span>
+            <span class="value">${movie.Actors}</span>
+          </div>
+
+          <div class="sub_sec">
+            <span class="field">Rating </span>
+            <span class="value">${movie.imdbRating}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    `
+    movieContainer.appendChild(card);
+}
+
+
+
+async function start(){
+  const movieId = getQueryParameter('id');
+  const movieData = await fetchMovie(movieId);
+  console.log("moviedata", movieData);
+  makeMovieCard(movieData);
+  // console.log("title", movieData.Title);
+}
+
+start();
